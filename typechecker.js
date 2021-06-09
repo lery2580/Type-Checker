a09c9b90e3a9c9b2ce3f444de0615552961a55ec
lery2580 <73690399+lery2580@users.noreply.github.com>
2021-06-09 23:06:50 +0800

    first commit

--- typechecker.js
+++ typechecker.js
@@ -1,9 +1,20 @@
 const sentenceTag = document.querySelector('input[type="text"]')
 
-const outputTag = document.querySelector("section.output")
+const outputTag = document.querySelector("textarea.output")
+
+const originalText = outputTag.value
 
 // when i type in my sentence tag, update the output tag accordingly
-
+// if there's no value,
 sentenceTag.addEventListener("keyup", function(){
-    outputTag.innerHTML = sentenceTag.value
+    if(this.value){
+        outputTag.value = this.value
+    } else {
+        outputTag.value = originalText
+    }
+})
+
+// when i type in my output tag, upadte the sentence tag accordingly
+outputTag.addEventListener("keyup", function(){
+    sentenceTag.value = this.value
 })
