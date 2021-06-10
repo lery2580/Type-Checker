c459a4114d1dc10e816b9e546e6fcf00f5a8172e
lery2580 <73690399+lery2580@users.noreply.github.com>
2021-06-10 22:35:40 +0800

    first commit

--- typechecker.js
+++ typechecker.js
@@ -1,5 +1,9 @@
 const sentenceTag = document.querySelector('input[type="text"]')
 
+const typesizeTag = document.querySelector('input[name="typesize"]')
+
+const typesizeOutput = document.querySelector("span.typesize-output")
+
 const outputTag = document.querySelector("textarea.output")
 
 const originalText = outputTag.value
@@ -17,4 +21,12 @@ sentenceTag.addEventListener("keyup", function(){
 // when i type in my output tag, upadte the sentence tag accordingly
 outputTag.addEventListener("keyup", function(){
     sentenceTag.value = this.value
+})
+
+// when i change my typesize slider, update the text to it and 
+// change the outputTag's font size
+
+typesizeTag.addEventListener("input", function(){
+    outputTag.style.fontSize = this.value + "px"
+    typesizeOutput.innerHTML = this.value + "px"
 })
