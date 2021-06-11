4231a339a4b94d7f9983047ff51a185905b08626
lery2580 <73690399+lery2580@users.noreply.github.com>
2021-06-11 22:48:20 +0800

    first commit

--- typechecker.js
+++ typechecker.js
@@ -4,6 +4,12 @@ const typesizeTag = document.querySelector('input[name="typesize"]')
 
 const typesizeOutput = document.querySelector("span.typesize-output")
 
+const lineheightTag = document.querySelector('input[name="Lineheight"]')
+
+const italicTag = document.querySelector('input[name="italic"]')
+
+const lineheightOutput = document.querySelector("span.lineheight-output")
+
 const outputTag = document.querySelector("textarea.output")
 
 const originalText = outputTag.value
@@ -29,4 +35,21 @@ outputTag.addEventListener("keyup", function(){
 typesizeTag.addEventListener("input", function(){
     outputTag.style.fontSize = this.value + "px"
     typesizeOutput.innerHTML = this.value + "px"
+})
+
+
+lineheightTag.addEventListener("input", function(){
+    outputTag.style.lineHeight = this.value 
+    lineheightOutput.innerHTML = this.value
+})
+
+// when i change my italic checkbox, update the font style to either
+// normal or italic if it's checked or not
+
+italicTag.addEventListener("change", function(){
+    if (this.checked) {
+        outputTag.style.fontStyle = "italic"
+    } else {
+        outputTag.style.fontStyle = "normal"
+    }
 })
